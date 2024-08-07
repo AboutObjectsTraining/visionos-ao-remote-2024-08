7706522e774765b52a4ef3443ae17cc4e9861a5e
Jonathan Lehr <jonathan@aboutobjects.com>
2024-08-07 17:38:04 -0400

    Fleshed out books browser.
    Added cover images from sample app.

--- Catalogs/Catalogs/BooksBrowser.swift
+++ Catalogs/Catalogs/BooksBrowser.swift
@@ -9,13 +9,88 @@ struct BooksBrowser: View {
     let viewModel: CatalogsViewModel
     
     var body: some View {
-        if !viewModel.hasBooks {
-            EmptyContentMessage(title: "No books.", message: "Tap the + button to add a book.")
-        } else {
-            List(viewModel.bookCatalog.books) { book in
-                Text(book.title)
+        Group {
+            if !viewModel.hasBooks {
+                EmptyContentMessage(title: "No books.", message: "Tap the + button to add a book.")
+            } else {
+                List(viewModel.bookCatalog.books) { book in
+                    BookCell(book: book)
+                }
             }
         }
     }
 }
 
+struct BookCell: View {
+    let book: Book
+    
+    private var bookInfo: some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Text(book.title)
+                .font(.headline)
+            Text("\(book.year)  \(book.author)")
+                .font(.subheadline)
+        }
+    }
+    
+    var body: some View {
+        HStack(spacing: 18) {
+            ThumbnailView(artworkURL: book.artworkURL, width: 70)
+            bookInfo
+            Spacer()
+            ProgressView(value: book.percentComplete)
+                .progressViewStyle(CompletionProgressViewStyle())
+        }
+    }
+}
+
+struct ThumbnailView: View {
+    let artworkURL: URL
+    let width: CGFloat
+    
+    var placeholder: some View {
+        ZStack {
+            Rectangle()
+                .fill(.regularMaterial)
+            Image(systemName: "photo")
+                .imageScale(.large)
+        }
+    }
+    
+    var body: some View {
+        AsyncImage(url: artworkURL) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } else if phase.error == nil {
+                ProgressView()
+            } else {
+                placeholder
+            }
+        }
+        .frame(width: width, height: 100)
+        .cornerRadius(5)
+    }
+}
+
+#Preview("Cell") {
+    BookCell(book: Book(title: "Foo", year: "1999", author: "Fred Smith"))
+        .padding(30)
+        .frame(width: 400)
+        .glassBackgroundEffect()
+}
+
+#Preview("List", windowStyle: .automatic, traits: .fixedLayout(width: 400, height: 600)) {
+    
+    let viewModel = CatalogsViewModel()
+    
+    BooksBrowser(viewModel: viewModel)
+        .onAppear {
+            if !viewModel.hasBooks {
+                Task {
+                    await viewModel.loadBooks()
+                }
+            }
+        }
+}
--- Catalogs/Catalogs/Model/Book.swift
+++ Catalogs/Catalogs/Model/Book.swift
@@ -21,7 +21,21 @@ import Foundation
     var author: String
     var percentComplete: Double
     
+    var artworkURL: URL {
+        let title = title.isEmpty ? "unknown" : title
+        let path = Bundle.main.path(forResource: title, ofType: "jpg")
+        return URL(fileURLWithPath: path ?? "")
+    }
+    
     var description: String {
         "\ntitle: \(title), year: \(year), author: \(author)"
+    }
+    
+    init(id: UUID = UUID(), title: String, year: String, author: String, percentComplete: Double = 0.0) {
+        self.id = id
+        self.title = title
+        self.year = year
+        self.author = author
+        self.percentComplete = percentComplete
     }
 }
